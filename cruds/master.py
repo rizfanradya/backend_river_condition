fd6281f45f3d8d940342086051ef8c2175e19052
rizfanradya <rizfankusuma@gmail.com>
2024-08-08 14:21:34 +0000

    improve

--- cruds/master.py
+++ cruds/master.py
@@ -1,39 +1,50 @@
 ﻿from sqlalchemy.orm import Session
 from models.master import MasterData
-from schemas.master import MasterDataCreateSchema, MasterDataResponseSchema
-from typing import Optional, List
-from sqlalchemy import or_
-from fastapi import UploadFile, File
+from schemas.master import MasterDataCreateSchema
+from typing import Optional
+from fastapi import UploadFile
 import os
 import uuid
 from PIL import Image as PILImage
 import aiofiles
 import datetime
+from utils import send_error_response
+
 
 async def save_file(file: UploadFile, destination: str):
     async with aiofiles.open(destination, 'wb') as out_file:
         content = await file.read()
         await out_file.write(content)
 
+
 def create_thumbnail(image_path: str, thumbnail_path: str, size=(128, 128)):
     with PILImage.open(image_path) as img:
         img.thumbnail(size)
         img.save(thumbnail_path)
 
+
 async def CreateData(session: Session, new_data: MasterDataCreateSchema, file: UploadFile):
     try:
         MAX_FILE_SIZE_BYTES = 2 * 1024 * 1024  # 2MB
-        allowed_content_types = ['image/jpeg', 'image/png']
+        allowed_content_types = ['image/jpeg', 'image/png', 'image/jpg']
         base_dir = os.path.dirname(os.path.abspath(__file__))
         upload_dir = os.path.join(base_dir, 'uploads')
         os.makedirs(upload_dir, exist_ok=True)
 
         if file.content_type not in allowed_content_types:
-            return {"error": "Wrong file type, only accept jpeg or png"}
+            # return {"error": "Wrong file type, only accept jpeg or png"}
+            send_error_response(
+                'Wrong file type, only accept jpeg or png',
+                'Wrong file type, only accept jpeg or png'
+            )
         if os.fstat(file.file.fileno()).st_size > MAX_FILE_SIZE_BYTES:
-            return {"error": "File too large, only accept file below 2MB"}
+            # return {"error": "File too large, only accept file below 2MB"}
+            send_error_response(
+                'File too large, only accept file below 2MB',
+                'File too large, only accept file below 2MB'
+            )
 
-        file_extension = file.filename.split('.')[-1]
+        file_extension = file.filename.split('.')[-1]  # type: ignore
         filename = f'{uuid.uuid4()}.{file_extension}'
         origin_filepath = os.path.join(upload_dir, 'origin', filename)
         thumbnail_filepath = os.path.join(upload_dir, 'thumbnails', filename)
@@ -58,19 +69,35 @@ async def CreateData(session: Session, new_data: MasterDataCreateSchema, file: U
         session.commit()
         session.refresh(new_data_info)
 
-        return MasterDataResponseSchema.from_orm(new_data_info)
+        return new_data_info
     except Exception as error:
-        return {"error": str(error)}
+        # return {"error": str(error)}
+        # raise HTTPException(
+        #     status_code=404,
+        #     detail={
+        #         "message": message,
+        #         "error": error,
+        #     }
+        # )
+        send_error_response(
+            str(error),
+            'Failed to create data'
+        )
+
 
 def GetAllData(session: Session, user_id: Optional[int] = None, limit: int = 10, offset: int = 0):
     query = session.query(MasterData)
     if user_id:
         query = query.filter(MasterData.user_id == user_id)
     data = query.offset(offset).limit(limit).all()
-    return [MasterDataResponseSchema.from_orm(item) for item in data]
+    for data_item in data:
+        data_item.upload_date_format = data_item.upload_date.strftime(
+            "%Y-%m-%d")
+    return data
+
 
 def GetDataById(session: Session, id: int):
     data = session.query(MasterData).filter(MasterData.id == id).first()
     if data is None:
         return {"error": f"Data with id {id} not found"}
-    return MasterDataResponseSchema.from_orm(data)
+    return data
--- routers/master.py
+++ routers/master.py
@@ -2,15 +2,16 @@
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from database import get_db
-from schemas.master import MasterDataCreateSchema, MasterDataResponseSchema
+from schemas.master import MasterDataCreateSchema
 from cruds.master import CreateData, GetAllData, GetDataById
-from typing import Optional, List
+from typing import Optional
 from cruds.user import TokenAuthorization
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/token")
 router = APIRouter()
 
-@router.post('/master', response_model=MasterDataResponseSchema)
+
+@router.post('/master')
 async def create_master_data(
     user_id: int = Form(...),
     description: str = Form(...),
@@ -21,7 +22,8 @@ async def create_master_data(
     token: str = Depends(oauth2_scheme)
 ):
     TokenAuthorization(session, token)
-    location = f'{longitude},{latitude}'  # Menggabungkan longitude dan latitude menjadi satu string
+    # Menggabungkan longitude dan latitude menjadi satu string
+    location = f'{longitude},{latitude}'
     new_data = MasterDataCreateSchema(
         user_id=user_id,
         description=description,
@@ -29,7 +31,8 @@ async def create_master_data(
     )
     return await CreateData(session, new_data, file)
 
-@router.get('/master', response_model=List[MasterDataResponseSchema])
+
+@router.get('/master')
 def get_master_data(
     user_id: Optional[int] = None,
     limit: int = 10,
@@ -40,7 +43,8 @@ def get_master_data(
     TokenAuthorization(session, token)
     return GetAllData(session, user_id, limit, offset)
 
-@router.get('/master/{id}', response_model=MasterDataResponseSchema)
+
+@router.get('/master/{id}')
 def get_master_data_by_id(
     id: int,
     session: Session = Depends(get_db),
